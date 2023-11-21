2752e9b2339e9ac668fb89bdfa78e7b2aeb6f66b
Jeremy Barber <devj23@proton.me>
2023-11-20 16:00:06 -0800

    Env file added

--- src/hooks/useFetchCats.tsx
+++ src/hooks/useFetchCats.tsx
@@ -1,17 +1,11 @@
 import useFetch from "./useFetch";
-/*
-
-const BASE_URL = "https://api.thecatapi.com/v1/";
-const API_KEY =
-  "live_sZihUO874TvRR4YK0SfuxFvSGynn8SlIFQ3rNCi4ToJNwwRCj34CfFGaQgC1yGeo";
-
-*/
 
 export default function useFetchCats(breedIds?: string[]) {
   const extra = breedIds ? `&breed_ids=${breedIds.join(",")}` : "";
   const { isError, isLoading, data } = useFetch(
-    "https://api.thecatapi.com/v1/images/search?limit=10&has_breeds=1&api_key=live_sZihUO874TvRR4YK0SfuxFvSGynn8SlIFQ3rNCi4ToJNwwRCj34CfFGaQgC1yGeo" +
-      extra
+    `https://api.thecatapi.com/v1/images/search?limit=10&has_breeds=1&api_key=${
+      import.meta.env.VITE_API_KEY
+    }${extra}`
   );
   return {
     isError,
